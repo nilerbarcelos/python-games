b1804a9b4360ac3f6ba8a949d733a80470cebe45
*Niler <nilerbarcelos@gmail.com>
2017-12-16 10:52:35 -0200

    Adicionando mensagem de resultado do jogo

--- forca.py
+++ forca.py
@@ -3,26 +3,33 @@ def jogar():
     print("Bem vindo no jogo de Forca! *****")
     print("*********************************")
 
-    palavra_secreta = "banana"
+    palavra_secreta = "banana".upper()
     letras_acertadas = ["_", "_", "_", "_", "_", "_"]
     enforcou = False
     acertou = False
+    erros = 0
 
     print(letras_acertadas)
 
     while(not enforcou and not acertou):
         chute = input("Qual letra?")
-        chute = chute.strip()
+        chute = chute.strip().upper()
 
-        index = 0
-        for letra in palavra_secreta:
-            if(chute.upper() == letra.upper()):
-                letras_acertadas[index] = letra
-            index = index + 1
+        if(chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if(chute == letra):
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
 
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
         print(letras_acertadas)
 
-    print("fim do jogo")
+    resultado = "Você Ganhou!" if acertou else "Você perdeu!"
+    print(resultado)
 
 
 if(__name__ == "__main__"):
