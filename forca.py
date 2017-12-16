540410b72a888cc05fa39bf649147f378abab2c9
*Niler <nilerbarcelos@gmail.com>
2017-12-16 09:06:47 -0200

    Iniciando jogo de forca

--- forca.py
+++ forca.py
@@ -3,6 +3,25 @@ def jogar():
     print("Bem vindo no jogo de Forca! *****")
     print("*********************************")
 
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+        chute = input("Qual letra?")
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute == letra):
+                print("Encontrei a letra {} na posicao {}".format(letra, index))
+            index = index + 1
+
+
+        print("Jogando ...")
+
+
+
+
     print("fim do jogo")
 
 
