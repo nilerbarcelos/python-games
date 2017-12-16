7a6ee88f6a13130f43191c08b792471ff7df7635
*Niler <nilerbarcelos@gmail.com>
2017-12-16 10:01:54 -0200

    Adicionando palavras na lista

--- forca.py
+++ forca.py
@@ -4,23 +4,23 @@ def jogar():
     print("*********************************")
 
     palavra_secreta = "banana"
+    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
     enforcou = False
     acertou = False
 
+    print(letras_acertadas)
+
     while(not enforcou and not acertou):
         chute = input("Qual letra?")
+        chute = chute.strip()
 
         index = 0
         for letra in palavra_secreta:
-            if(chute == letra):
-                print("Encontrei a letra {} na posicao {}".format(letra, index))
+            if(chute.upper() == letra.upper()):
+                letras_acertadas[index] = letra
             index = index + 1
 
-
-        print("Jogando ...")
-
-
-
+        print(letras_acertadas)
 
     print("fim do jogo")
 
